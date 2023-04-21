1fad2872d4348af29ec3a2ad22d551a2d75b9afd
Pepijn98 <pepijn@vdbroek.dev>
2023-04-21 18:14:12 +0200

    Update user/Post.ts

--- src/api/routes/user/Post.ts
+++ src/api/routes/user/Post.ts
@@ -1,8 +1,17 @@
 import Base from "~/api/Base";
 import Router from "~/api/Router";
-import { httpError } from "~/utils/general";
+import bcrypt from "bcrypt";
 
 import { Request, Response } from "express";
+import Users, { PublicUser, Role, User } from "~/models/User";
+import { generateToken, httpError } from "~/utils/general";
+
+interface Signup {
+    email: string;
+    username: string;
+    password: string;
+    role: Role;
+}
 
 export default class extends Base {
     constructor(controller: Router) {
@@ -18,16 +27,33 @@ export default class extends Base {
 
     async run(req: Request, res: Response): Promise<void> {
         try {
-            if (!req.user) {
-                res.status(404).json(httpError[404]);
+            const body: Signup = req.body;
+            if (!body.email || !body.username || !body.password) {
+                res.status(400).json(httpError[400]);
                 return;
             }
+
+            const hashed = await bcrypt.hash(body.password, 14);
+            const token = await generateToken();
+
+            const newUser = new Users({
+                email: body.email,
+                username: body.username,
+                password: hashed,
+                token: token,
+                role: body.role,
+                createdAt: new Date()
+            } satisfies User);
+
+            await newUser.save();
 
             res.status(200).json({
                 statusCode: 200,
                 statusMessage: "OK",
-                message: "Success",
-                data: {}
+                message: "Successfully created user",
+                data: {
+                    user: new PublicUser(newUser)
+                }
             });
         } catch (error) {
             this.handleException(res, error);
