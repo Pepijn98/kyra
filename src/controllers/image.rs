4e939e3f688d4aa0bcac8f9404590d67ac29b5af
Pepijn98 <pepijn@vdbroek.dev>
2023-07-18 17:40:28 +0200

    Update match statements (less nesting, more readable)

--- src/controllers/image.rs
+++ src/controllers/image.rs
@@ -38,123 +38,8 @@ pub async fn post_image(
     multipart: Multipart,
 ) -> impl IntoResponse {
     let multipart_data = image_from_multipart(multipart).await;
-    match multipart_data {
-        Ok(content) => {
-            if content.len() <= 0 {
-                return (
-                    StatusCode::BAD_REQUEST,
-                    Json(json!(Response::<()> {
-                        success: false,
-                        message: String::from("Failed to find image in request data"),
-                        data: None
-                    })),
-                );
-            }
-
-            let data = Cursor::new(content);
-            let reader = Reader::new(data).with_guessed_format().unwrap();
-
-            let file_ext = reader.format().unwrap_or(ImageFormat::Jpeg);
-
-            let thumbnail_path = format!("./data/thumbnails/{id}");
-            let image_path = format!("./data/images/{id}");
-
-            let thumb_dir_result = fs::create_dir_all(&thumbnail_path);
-            match thumb_dir_result {
-                Ok(_) => {}
-                Err(_) => {
-                    return (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!(Response::<()> {
-                            success: false,
-                            message: String::from("Failed to create thumbnail directory"),
-                            data: None
-                        })),
-                    )
-                }
-            };
-
-            let image_dir_result = fs::create_dir_all(&image_path);
-            match image_dir_result {
-                Ok(_) => {}
-                Err(_) => {
-                    return (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!(Response::<()> {
-                            success: false,
-                            message: String::from("Failed to create image directory"),
-                            data: None
-                        })),
-                    )
-                }
-            }
-
-            match reader.decode() {
-                Ok(dyn_image) => {
-                    // Creates a compressed smaller version of the image
-                    let thumbnail = dyn_image.thumbnail(360, 360);
-
-                    // Limit images to 2000x2000px, Keeps aspec ration and fits the maximum possible size between 2000x2000
-                    let image = if dyn_image.height() > 2000 || dyn_image.width() > 2000 {
-                        dyn_image.resize(2000, 2000, FilterType::Lanczos3)
-                    } else {
-                        dyn_image
-                    };
-
-                    let file_name = nanoid!(7, &ALPHANUMERIC);
-
-                    let thumb_result = thumbnail.save(format!("{thumbnail_path}/{file_name}.jpg"));
-                    match thumb_result {
-                        Ok(_) => {}
-                        Err(_) => {
-                            return (
-                                StatusCode::INTERNAL_SERVER_ERROR,
-                                Json(json!(Response::<()> {
-                                    success: false,
-                                    message: String::from("Failed to save thumbnail"),
-                                    data: None
-                                })),
-                            )
-                        }
-                    }
-
-                    let ext = file_ext.extensions_str()[0];
-                    let image_result = &image.save(format!("{image_path}/{file_name}.{ext}"));
-                    return match image_result {
-                        Ok(_) => {
-                            let hash = md5::compute(&image.into_bytes());
-                            /* TODO: Save entry in database {name, ext, hash, uploader_id, created_on} */
-                            return (
-                                StatusCode::CREATED,
-                                Json(json!(Response::<()> {
-                                    success: true,
-                                    message: String::from("Image successfully saved"),
-                                    data: None
-                                })),
-                            );
-                        }
-                        Err(_) => (
-                            StatusCode::INTERNAL_SERVER_ERROR,
-                            Json(json!(Response::<()> {
-                                success: false,
-                                message: String::from("Failed to save image"),
-                                data: None
-                            })),
-                        ),
-                    };
-                }
-                Err(_) => {
-                    return (
-                        StatusCode::BAD_REQUEST,
-                        Json(json!(Response::<()> {
-                            success: false,
-                            message: String::from("Invalid image format"),
-                            data: None
-                        })),
-                    )
-                }
-            }
-        }
+    let content = match multipart_data {
+        Ok(data) => data,
         Err(_) => {
             return (
                 StatusCode::BAD_REQUEST,
@@ -163,7 +48,112 @@ pub async fn post_image(
                     message: String::from("Failed to get image from form-data"),
                     data: None
                 })),
-            )
+            );
+        }
+    };
+
+    if content.len() <= 0 {
+        return (
+            StatusCode::BAD_REQUEST,
+            Json(json!(Response::<()> {
+                success: false,
+                message: String::from("Failed to find image in request data"),
+                data: None
+            })),
+        );
+    }
+
+    let data = Cursor::new(content);
+    let reader = Reader::new(data).with_guessed_format().unwrap();
+
+    let file_ext = reader.format().unwrap_or(ImageFormat::Jpeg);
+
+    let thumbnail_path = format!("./data/thumbnails/{id}");
+    let image_path = format!("./data/images/{id}");
+
+    if let Err(_) = fs::create_dir_all(&thumbnail_path) {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!(Response::<()> {
+                success: false,
+                message: String::from("Failed to create thumbnail directory"),
+                data: None,
+            })),
+        );
+    }
+
+    if let Err(_) = fs::create_dir_all(&image_path) {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!(Response::<()> {
+                success: false,
+                message: String::from("Failed to create image directory"),
+                data: None
+            })),
+        );
+    }
+
+    let image = match reader.decode() {
+        Ok(dyn_image) => dyn_image,
+        Err(_) => {
+            return (
+                StatusCode::BAD_REQUEST,
+                Json(json!(Response::<()> {
+                    success: false,
+                    message: String::from("Invalid image format"),
+                    data: None
+                })),
+            );
+        }
+    };
+
+    // Creates a compressed smaller version of the image
+    let thumbnail = image.thumbnail(360, 360);
+
+    // Limit images to 2000x2000px, Keeps aspec ration and fits the maximum possible size between 2000x2000
+    let image = if image.height() > 2000 || image.width() > 2000 {
+        image.resize(2000, 2000, FilterType::Lanczos3)
+    } else {
+        image
+    };
+
+    // Create random filename
+    let file_name = nanoid!(7, &ALPHANUMERIC);
+
+    if let Err(_) = thumbnail.save(format!("{thumbnail_path}/{file_name}.jpg")) {
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(json!(Response::<()> {
+                success: false,
+                message: String::from("Failed to save thumbnail"),
+                data: None
+            })),
+        );
+    };
+
+    let ext = file_ext.extensions_str()[0];
+    match &image.save(format!("{image_path}/{file_name}.{ext}")) {
+        Ok(_) => {
+            let hash = md5::compute(&image.into_bytes());
+            /* TODO: Save entry in database {name, ext, hash, uploader_id, created_on} */
+            return (
+                StatusCode::CREATED,
+                Json(json!(Response::<()> {
+                    success: true,
+                    message: String::from("Image successfully saved"),
+                    data: None
+                })),
+            );
+        }
+        Err(_) => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!(Response::<()> {
+                    success: false,
+                    message: String::from("Failed to save image"),
+                    data: None
+                })),
+            );
         }
     };
 }
