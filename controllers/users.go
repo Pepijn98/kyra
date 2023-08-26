7f1c7132e036e65c2be50e0c996de9d918c358a1
Pepijn98 <pepijn@vdbroek.dev>
2023-08-26 14:50:44 +0200

    Check error using sql package variable instead of exact string

--- controllers/users.go
+++ controllers/users.go
@@ -9,7 +9,7 @@ import (
 )
 
 func GetUsers(c *fiber.Ctx, db *sql.DB) error {
-	rows, err := db.Query("SELECT id, email, username, token, role, created_at FROM users")
+	rows, err := db.Query(`SELECT id, email, username, token, role, created_at FROM users`)
 	if err != nil {
 		return c.Status(500).JSON(models.ErrorResponse{
 			Success: false,
@@ -23,7 +23,7 @@ func GetUsers(c *fiber.Ctx, db *sql.DB) error {
 		var user models.User
 
 		if err := rows.Scan(&user.Id, &user.Email, &user.Username, &user.Token, &user.Role, &user.CreatedAt); err != nil {
-			if err.Error() == "sql: no rows in result set" {
+			if err == sql.ErrNoRows {
 				return c.Status(404).JSON(models.ErrorResponse{
 					Success: false,
 					Error:   "No users found",
@@ -45,11 +45,11 @@ func GetUsers(c *fiber.Ctx, db *sql.DB) error {
 }
 
 func GetUser(c *fiber.Ctx, db *sql.DB) error {
-	row := db.QueryRow("SELECT id, email, username, token, role, created_at FROM users WHERE id = ?", c.Params("id"))
+	row := db.QueryRow(`SELECT id, email, username, token, role, created_at FROM users WHERE id = ?;`, c.Params("id"))
 
 	var user models.User
 	if err := row.Scan(&user.Id, &user.Email, &user.Username, &user.Token, &user.Role, &user.CreatedAt); err != nil {
-		if err.Error() == "sql: no rows in result set" {
+		if err == sql.ErrNoRows {
 			return c.Status(404).JSON(models.ErrorResponse{
 				Success: false,
 				Error:   "User not found",
